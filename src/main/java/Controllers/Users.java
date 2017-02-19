82eca2318c5833328e416d0ffe4340813f73f952
Giovanni Lenguito <giovanni16.gl@gmail.com>
2017-02-19 17:18:01 +0000

    Revert "git cache cleared"
    This reverts commit 498aeb1a7ccc578609ce19e782d28f7f1d908b98.

--- src/main/java/Controllers/Users.java
+++ src/main/java/Controllers/Users.java
@@ -1,20 +1,37 @@
 package Controllers;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import java.io.IOException;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.Path;
 
 /**
  * Created by giovannilenguito on 19/02/2017.
  */
-// The Java class will be hosted at the URI path "/users"
-@Path("/users")
+// The Java class will be hosted at the URI path "/helloworld"
+@Path("/helloworld")
 public class Users {
+    // The Java method will process HTTP GET requests
+    @GET
+    // The Java method will produce content identified by the MIME Media type "text/plain"
+    @Produces("text/plain")
+    public String getClichedMessage() {
+        // Return some cliched textual content
+        return "Hello World";
+    }
 
-    @POST
-    @Produces("application/json")
-    public Response postUser() {
-        System.out.println("here");
-        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("{'message': 'here'}").header("Access-Control-Allow-Origin", "*").build();
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServerFactory.create("http://localhost:9998/");
+        server.start();
+
+        System.out.println("Server running");
+        System.out.println("Visit: http://localhost:9998/helloworld");
+        System.out.println("Hit return to stop...");
+        System.in.read();
+        System.out.println("Stopping server");
+        server.stop(0);
+        System.out.println("Server stopped");
     }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -21,7 +21,6 @@ public class main {
     public static void main(String[] args) throws IOException {
         //Set up the http server
         HttpServer server = HttpServerFactory.create("http://localhost:3002/");
-
         server.start();
 
         //Print messages
